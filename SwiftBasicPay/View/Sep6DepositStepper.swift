a197a9f0c0a685061c311665ba453b12fd290077
Christian Rogobete <c.rogobete@soneso.com>
2025-07-31 13:51:07 +0200

    feat(transfer): add anchor kyc handling

--- SwiftBasicPay/View/Sep6DepositStepper.swift
+++ SwiftBasicPay/View/Sep6DepositStepper.swift
@@ -21,7 +21,12 @@ struct Sep6DepositStepper: View {
     @State private var anchorHasEnabledFeeEndpoint = false
     @State private var collectedTransferDetails:[String] = []
     @State private var transferAmount:String = ""
-    @State private var transferFieldsValidationError:String? = nil
+    @State private var transferFieldsError:String? = nil
+    @State private var isLoadingKyc = false
+    @State private var kycLoadingText:String = ""
+    @State private var kycInfo:GetCustomerResponse? = nil
+    @State private var kycFieldsError:String? = nil
+    @State private var collectedKycDetails:[String] = []
 
     var body: some View {
         NavigationStack {
@@ -37,47 +42,46 @@ struct Sep6DepositStepper: View {
                     // Amount is always needed
                     amountInputField
                     transferFields
-                    if let error = transferFieldsValidationError {
+                    if let error = transferFieldsError {
                         Utils.divider
                         Text("\(error)").font(.footnote).foregroundStyle(.red).frame(maxWidth: .infinity, alignment: .leading)
                     }
                     
                 } else if currentStep == 2 {
-                    Text("KYC")
+                    if isLoadingKyc {
+                        HStack {
+                            Utils.progressView
+                            Text(kycLoadingText).padding(.leading).frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    else if let info = kycInfo, !isLoadingKyc {
+                        if info.sep12Status == Sep12Status.neesdInfo {
+                            Text("The anchor needs following of your KYC data:").font(.subheadline)
+                            kycFields
+                        } else if(info.sep12Status == Sep12Status.accepted) {
+                            Text("Your KYC data has been accepted by the anchor.").font(.subheadline)
+                            Button("Delete") {
+                                Task {
+                                    await deleteKYCData()
+                                }
+                            }
+                        } else if(info.sep12Status == Sep12Status.processing) {
+                            Text("Your KYC data is currently being processed by the anchor.").font(.subheadline)
+                        } else if(info.sep12Status == Sep12Status.rejected) {
+                            Text("Your KYC data has been rejected by the anchor.").font(.subheadline)
+                        }
+                        if let error = kycFieldsError {
+                            Utils.divider
+                            Text("\(error)").font(.footnote).foregroundStyle(.red).frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
                 } else if currentStep == 3 {
                     Text("Fee")
                 } else if currentStep == 4 {
                     Text("Summary")
                 }
                 Utils.divider
-                HStack {
-                    Button("Previous") {
-                        if currentStep > 1 {
-                            currentStep -= 1
-                        }
-                    }
-                    .disabled(currentStep == 1)
-
-                    Spacer()
-
-                    if currentStep < 4 {
-                        Button("Next") {
-                            if currentStep == 1 {
-                                if validateTransferFields() {
-                                    currentStep += 1
-                                }
-                            }
-                            else if currentStep < 4 {
-                                currentStep += 1
-                            }
-                        }
-                    } else {
-                        Button("Submit") {
-                            // Handle submission logic here
-                        }
-                    }
-                }
-                .padding(.trailing)
+                navigationButtons
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
             .padding()
@@ -95,7 +99,7 @@ struct Sep6DepositStepper: View {
             if let dinfo = depositInfo.fieldsInfo {
                 for key in dinfo.keys {
                     if let val = dinfo[key] {
-                        if val.choices != nil {
+                        if val.choices != nil && !val.choices!.isEmpty {
                             self.collectedTransferDetails.append(selectItem)
                         } else {
                             self.collectedTransferDetails.append("")
@@ -104,6 +108,46 @@ struct Sep6DepositStepper: View {
                 }
             }
         })
+    }
+    
+    private var navigationButtons: some View {
+        HStack {
+            Button("Previous") {
+                if currentStep > 1 {
+                    currentStep -= 1
+                }
+            }
+            .disabled(currentStep == 1)
+
+            Spacer()
+
+            if currentStep < 4 {
+                Button("Next") {
+                    if currentStep == 1 && validateTransferFields(){
+                        currentStep += 1
+                        Task {
+                            await loadKYCData()
+                        }
+                    } else if currentStep == 2 {
+                        if (kycInfo?.sep12Status == Sep12Status.neesdInfo && validateKycFields()) {
+                            Task {
+                                await submitKYCData()
+                            }
+                        } else if (kycInfo?.sep12Status == Sep12Status.accepted) {
+                            currentStep += 1
+                        }
+                    }
+                    else if currentStep > 2 && currentStep < 4 {
+                        currentStep += 1
+                    }
+                }
+            } else {
+                Button("Submit") {
+                    // Handle submission logic here
+                }
+            }
+        }
+        .padding(.trailing)
     }
     
     private var amountInputField: some View  {
@@ -120,7 +164,7 @@ struct Sep6DepositStepper: View {
             
             Text("min: \(min) \(max)").font(.subheadline).font(.caption)
                 .fontWeight(.light).italic()
-                .foregroundColor(.secondary).frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.orange).frame(maxWidth: .infinity, alignment: .leading)
         }
     }
     
@@ -132,7 +176,7 @@ struct Sep6DepositStepper: View {
         return depositInfo.maxAmount
     }
     
-    var fieldInfos: [TransferFieldInfo] {
+    var transferFieldInfos: [TransferFieldInfo] {
         var info:[TransferFieldInfo] = []
         if let dinfo = depositInfo.fieldsInfo {
             for key in dinfo.keys {
@@ -143,7 +187,7 @@ struct Sep6DepositStepper: View {
         }
         return info
     }
-    
+        
     private func indexForTransferFieldKey(key:String) -> Int {
         if let dinfo = depositInfo.fieldsInfo {
             for (index, infoKey) in dinfo.keys.enumerated() {
@@ -158,27 +202,29 @@ struct Sep6DepositStepper: View {
     private var transferFields : some View {
         VStack {
             if !collectedTransferDetails.isEmpty {
-                ForEach(fieldInfos, id: \.key) { info in
-                    let optional = info.optional ? "(optional)" : ""
-                    if let choices = info.info.choices {
-                        HStack {
-                            Text("\(info.key) \(optional):")
-                            Picker("select \(info.key)", selection: $collectedTransferDetails[indexForTransferFieldKey(key: info.key)]) {
-                                Text(selectItem).italic().foregroundColor(.black).tag(selectItem)
-                                ForEach(choices, id: \.self) { choice in
-                                    Text(choice).italic().foregroundColor(.black).tag(choice)
-                                }
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                            
+                ScrollView {
+                    ForEach(transferFieldInfos, id: \.key) { info in
+                        
+                        if let choices = info.info.choices, !choices.isEmpty {
+                            HStack {
+                                Text("\(info.key):").font(.subheadline).fontWeight(.light)
+                                Picker("select \(info.key)", selection: $collectedTransferDetails[indexForTransferFieldKey(key: info.key)]) {
+                                    Text(selectItem).italic().foregroundColor(.black).tag(selectItem)
+                                    ForEach(choices, id: \.self) { choice in
+                                        Text(choice).italic().foregroundColor(.black).tag(choice)
+                                    }
+                                }.frame(maxWidth: .infinity, alignment: .leading)
+                                
+                            }
+                        } else {
+                            TextField("\(info.key)", text: $collectedTransferDetails[indexForTransferFieldKey(key: info.key)])
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
                         }
-                    } else {
-                        TextField("Enter \(info.key) \(optional)", text: $collectedTransferDetails[indexForTransferFieldKey(key: info.key)])
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                    }
-                    if let description = info.info.description {
-                        Text(description).font(.subheadline).font(.caption)
+                        let optional = info.optional ? "(optional)" : "*"
+                        Text("\(optional) \(info.info.description ?? "")").font(.subheadline).font(.caption)
                             .fontWeight(.light).italic()
-                            .foregroundColor(.secondary).frame(maxWidth: .infinity, alignment: .leading)
+                            .foregroundColor(info.optional ? .secondary : .orange).frame(maxWidth: .infinity, alignment: .leading)
+                        Spacer(minLength: 10)
                     }
                 }
             }
@@ -186,17 +232,17 @@ struct Sep6DepositStepper: View {
     }
     
     private func validateTransferFields() -> Bool {
-        transferFieldsValidationError = nil
+        transferFieldsError = nil
         if transferAmount.isEmpty {
-            transferFieldsValidationError = "Amount is not optional"
+            transferFieldsError = "Amount is not optional"
             return false
         }
         guard let amount = Double(transferAmount) else {
-            transferFieldsValidationError = "Invalid amount"
+            transferFieldsError = "Invalid amount"
             return false
         }
         if let maxAmount = maxAmount, amount > maxAmount {
-            transferFieldsValidationError = "Amount is to high."
+            transferFieldsError = "Amount is to high."
             return false
         }
         
@@ -207,7 +253,7 @@ struct Sep6DepositStepper: View {
                 if !optional {
                     let val = collectedTransferDetails[index]
                     if val.isEmpty || val == selectItem {
-                        transferFieldsValidationError = "\(infoKey) is not optional"
+                        transferFieldsError = "\(infoKey) is not optional"
                         return false
                     }
                 }
@@ -215,6 +261,153 @@ struct Sep6DepositStepper: View {
         }
         
         return true
+    }
+    
+    var kycFieldInfos: [KycFieldInfo] {
+        var info:[KycFieldInfo] = []
+        if let dinfo = kycInfo?.fields {
+            for key in dinfo.keys {
+                if let val = dinfo[key] {
+                    info.append(KycFieldInfo(key: key, info: val))
+                }
+            }
+        }
+        return info
+    }
+    
+    private func indexForKycFieldKey(key:String) -> Int {
+        if let dinfo = kycInfo?.fields {
+            for (index, infoKey) in dinfo.keys.enumerated() {
+                if key == infoKey {
+                    return index
+                }
+            }
+        }
+        return -1
+    }
+    
+    private var kycFields : some View {
+        VStack {
+            if !collectedKycDetails.isEmpty {
+                ScrollView {
+                    ForEach(kycFieldInfos, id: \.key) { info in
+                        if let choices = info.info.choices, !choices.isEmpty {
+                            HStack {
+                                Text("\(info.key):").font(.subheadline).fontWeight(.light)
+                                Picker("select \(info.key)", selection: $collectedKycDetails[indexForKycFieldKey(key: info.key)]) {
+                                    Text(selectItem).italic().foregroundColor(.black).tag(selectItem)
+                                    ForEach(choices, id: \.self) { choice in
+                                        Text(choice).italic().foregroundColor(.black).tag(choice)
+                                    }
+                                }.frame(maxWidth: .infinity, alignment: .leading)
+                                
+                            }
+                        } else {
+                            TextField("\(info.key)", text: $collectedKycDetails[indexForKycFieldKey(key: info.key)])
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                        }
+                        let optional = info.optional ? "(optional)" : "*"
+                        Text("\(optional) \(info.info.description ?? "")").font(.caption)
+                            .fontWeight(.light).italic()
+                            .foregroundColor(info.optional ? .secondary : .orange).frame(maxWidth: .infinity, alignment: .leading)
+                        Spacer(minLength: 20)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func validateKycFields() -> Bool {
+        kycFieldsError = nil
+        if let dinfo = kycInfo?.fields {
+            for (index, infoKey) in dinfo.keys.enumerated() {
+                let field = dinfo[infoKey]
+                let optional = field?.optional ?? false
+                if !optional {
+                    let val = collectedKycDetails[index]
+                    if val.isEmpty || val == selectItem {
+                        kycFieldsError = "\(infoKey) is not optional"
+                        return false
+                    }
+                }
+            }
+        }
+        
+        return true
+    }
+    
+    private func loadKYCData() async {
+        kycLoadingText = "Loading KYC data"
+        isLoadingKyc = true
+        kycInfo = nil
+        kycFieldsError =  nil
+        do {
+            let sep12 = try await anchoredAsset.anchor.sep12(authToken: authToken)
+            kycInfo = try await sep12.getByAuthTokenOnly()
+            print("kyc status: \(kycInfo!.sep12Status.rawValue)")
+            collectedKycDetails = []
+            if let dinfo = kycInfo?.fields {
+                for key in dinfo.keys {
+                    if let val = dinfo[key] {
+                        if val.choices != nil && !val.choices!.isEmpty {
+                            self.collectedKycDetails.append(selectItem)
+                        } else {
+                            self.collectedKycDetails.append("")
+                        }
+                    }
+                }
+            }
+        } catch {
+            kycFieldsError = "Could not load SEP-12 (KYC) info from anchor: \(error.localizedDescription)"
+        }
+        isLoadingKyc = false
+    }
+    
+    private func deleteKYCData() async {
+        kycLoadingText = "Deleting KYC data"
+        isLoadingKyc = true
+        kycInfo = nil
+        kycFieldsError =  nil
+        do {
+            let sep12 = try await anchoredAsset.anchor.sep12(authToken: authToken)
+            try await sep12.delete(account: authToken.account)
+            await loadKYCData()
+        } catch {
+            kycFieldsError = "Could not delete your KYC data from anchor: \(error.localizedDescription)"
+        }
+        isLoadingKyc = false
+    }
+    
+    private func submitKYCData() async {
+        kycLoadingText = "Submitting KYC data"
+        isLoadingKyc = true
+        do {
+            let sep12 = try await anchoredAsset.anchor.sep12(authToken: authToken)
+            if let customerId = kycInfo?.id {
+                // known anchor customer
+                let _ = try await sep12.update(id: customerId, sep9Info: preparedKycData)
+            } else {
+                // new anchor customer
+                let _ = try await sep12.add(sep9Info: preparedKycData)
+            }
+            try await Task.sleep(nanoseconds: 5_000_000_000) // wait 5 sec.
+            await loadKYCData()
+        } catch {
+            kycFieldsError = "Could not submit your KYC data to the anchor: \(error.localizedDescription)"
+        }
+        isLoadingKyc = false
+    }
+    
+    private var preparedKycData : [String:String] {
+        var result:[String:String]  = [:]
+        if let dinfo = kycInfo?.fields, !collectedKycDetails.isEmpty {
+            for (index, key) in dinfo.keys.enumerated() {
+                if (collectedKycDetails.count > index) {
+                    result[key] = collectedKycDetails[index]
+                }
+            }
+        }
+        return result
     }
 }
 
@@ -241,6 +434,27 @@ public class TransferFieldInfo: Hashable, Identifiable {
     }
     
     public static func == (lhs: TransferFieldInfo, rhs: TransferFieldInfo) -> Bool {
+        lhs.key == rhs.key
+    }
+}
+
+public class KycFieldInfo: Hashable, Identifiable {
+    let key:String
+    let info:Field
+    
+    internal init(key: String, info: Field) {
+        self.key = key
+        self.info = info
+    }
+    
+    public var optional : Bool {
+        return info.optional ?? false
+    }
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(key)
+    }
+    
+    public static func == (lhs: KycFieldInfo, rhs: KycFieldInfo) -> Bool {
         lhs.key == rhs.key
     }
 }
